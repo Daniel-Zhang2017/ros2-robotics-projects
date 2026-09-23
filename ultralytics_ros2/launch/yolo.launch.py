86e9d6e29c07df67c6cad696624ed43a387c3f26
Daniel-Zhang2017 <155353044+Daniel-Zhang2017@users.noreply.github.com>
2026-09-23 23:13:12 +0800

    Update yolo.launch.py

--- ultralytics_ros2/launch/yolo.launch.py
+++ ultralytics_ros2/launch/yolo.launch.py
@@ -8,7 +8,7 @@ def generate_launch_description():
             executable='detection_node',
             name='yolo_detector',
             parameters=[
-                {'model': '/home/wheeltec/wheeltec_ros2/src/ultralytics_ros2/model/traffic_sign_new1.pt'},
+                {'model': '/home/wheeltec/wheeltec_ros2/src/ultralytics_ros2/model/yolov8s-pose.pt'},
                 {'input_image_topic': '/image_raw'},
                 {'enable_cuda': True},
                 {'conf_threshold': 0.5}
