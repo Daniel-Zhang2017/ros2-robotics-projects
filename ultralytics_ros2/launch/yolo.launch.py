30f39c484d13ca75907d159edc6d3492433e3dcb
Daniel-Zhang2017 <155353044+Daniel-Zhang2017@users.noreply.github.com>
2026-09-24 11:38:59 +0800

    Update yolo.launch.py

--- ultralytics_ros2/launch/yolo.launch.py
+++ ultralytics_ros2/launch/yolo.launch.py
@@ -1,7 +1,5 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-import os
-
 
 def generate_launch_description():
     return LaunchDescription([
@@ -9,17 +7,11 @@ def generate_launch_description():
             package='ultralytics_ros2',
             executable='detection_node',
             name='yolo_detector',
-            output='screen',
-            emulate_tty=True,
-            parameters=[{
-                'model': os.path.expanduser(
-                    '~/wheeltec_ros2/src/ultralytics_ros2/model/yolov8s-pose.pt'
-                ),
-                'input_image_topic': '/image_raw',
-                'enable_cuda': True,
-                'conf_threshold': 0.5,
-                'infer_period': 0.08,
-                'imgsz': 640,
-            }],
+            parameters=[
+                {'model': '/home/wheeltec/wheeltec_ros2/src/ultralytics_ros2/model/yolov8s.pt'},
+                {'input_image_topic': '/image_raw'},
+                {'enable_cuda': True},
+                {'conf_threshold': 0.5}
+            ]
         )
     ])
